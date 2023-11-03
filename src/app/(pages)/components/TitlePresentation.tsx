8cc6229f7a158e5e65d193ec5a465a2e80acc324
GonzaloAxelCode <gonzaloaxelcode@gmail.com>
2023-11-03 18:45:56 -0500

    asdas

--- src/app/(pages)/components/TitlePresentation.tsx
+++ src/app/(pages)/components/TitlePresentation.tsx
@@ -12,69 +12,37 @@ const TitlePresentation = () => {
     <div className="-mt-12 mb-12 md:mt-0 md:mb-24">
       <div className="content-wrapper">
         <div className="flex items-center justify-center py-8">
-          <blockquote className="flex gap-2 pt-2 text-4xl sm:text-4xl text-graydarksoft md:text-4xl lg:pt-0 lg:text-5xl dark:text-gray-400">
-            <svg
-              xmlns="http://www.w3.oarg/2000/svg"
-              viewBox="0 0 20 20"
-              role="img"
-              fill="currentColor"
-              className="-mt-1 h-10 text-gray-300 md:-mt-3  dark:text-white"
-            >
-              <path d="M7.27273 16.3833H0L5.30713 4H10.0737L7.27273 16.3833ZM17.199 16.3833H9.92629L15.2334 4H20L17.199 16.3833Z" />
-            </svg>
-            <span className="flex flex-col">
-              <span className="leading-[1.15]">
-                <em>Hola, soy</em>{" "}
-              </span>
-              <span className="flex items-center gap-2 leading-[1.15] lg:gap-4">
-                <span className="mt-1 h-0.5 w-8 rounded-full bg-gray-400 lg:h-1 lg:w-24 dark:bg-gray-600" />
-                <span>
-                  <strong className="font-extrabold text-graydark dark:text-gray-300">
-                    Gonzalo 
-                  </strong>
-                  <strong className="font-extrabold text-graydark dark:text-gray-300">
-                    Axel
-                  </strong>
-                  <strong className="font-extrabold text-graydarksoft dark:text-gray-300"></strong>
-                </span>
-                <span className="mt-1 h-0.5 w-6 rounded-full bg-gray-400 lg:h-1 lg:w-14 dark:bg-gray-600" />
-              </span>
-              <span className="leading-[1.15]">
-                desarrollador{" "}
-                <strong className="relative font-extrabol">
-                  <span className="absolute -left-0.5 right-0 top-1 bottom-0 z-[-1] rounded-md bg-gray-100 px-1 lg:-left-1.5 lg:-right-0.5 lg:top-2 lg:bottom-0 dark:bg-gray-800" />
-                  <br className="md:hidden" />
-                  <span className={cn(gradientTextClass)}>
-                    <TypeAnimation
-                      preRenderFirstString={true}
-                      sequence={[
-                        "frontend",
-                        800,
-                        () =>
-                          setGradientTextClass(
-                            "inline leading-[0] bg-gradient-to-br bg-clip-text text-transparent from-[#ff9900] via-[#ff2f61] to-[#ff5e62]"
-                          ),
-                        "backend",
-                        800,
-                        () =>
-                          setGradientTextClass(
-                            "inline leading-[0] bg-gradient-to-br bg-clip-text text-transparent from-[#17a2b8] via-[#7b74a7] to-[#3b5c7f]"
-                          ),
-                        "fullstack",
-                        1000,
-                        () =>
-                          setGradientTextClass(
-                            "inline leading-[0] bg-gradient-to-br bg-clip-text text-transparent from-[#2ecc71] via-[#19b5fe] to-[#2e86de]"
-                          ),
-                      ]}
-                      speed={50}
-                      repeat={Infinity}
-                    />
-                  </span>
-                </strong>
-              </span>
+          <h1 className="font-headings leading-normal  scroll-mt-[120px] text-6xl lg:text-7xl 2xl:text-8xl font-bold title-limiter">
+            Soy Gonzalo Axel <br />
+            un desarrollador de aplicaciones <br />
+            <span className={cn(gradientTextClass)}>
+              <TypeAnimation
+                preRenderFirstString={true}
+                sequence={[
+                  "frontend",
+                  800,
+                  () =>
+                    setGradientTextClass(
+                      "inline leading-[0] bg-gradient-to-br bg-clip-text text-transparent from-[#ff9900] via-[#ff2f61] to-[#ff5e62]"
+                    ),
+                  "backend",
+                  800,
+                  () =>
+                    setGradientTextClass(
+                      "inline leading-[0] bg-gradient-to-br bg-clip-text text-transparent from-[#17a2b8] via-[#7b74a7] to-[#3b5c7f]"
+                    ),
+                  "fullstack",
+                  1000,
+                  () =>
+                    setGradientTextClass(
+                      "inline leading-[0] bg-gradient-to-br bg-clip-text text-transparent from-[#2ecc71] via-[#19b5fe] to-[#2e86de]"
+                    ),
+                ]}
+                speed={50}
+                repeat={Infinity}
+              />
             </span>
-          </blockquote>
+          </h1>
         </div>
       </div>
       <div className="ml-10  text-md flex flex-col ">
@@ -83,13 +51,45 @@ const TitlePresentation = () => {
           Apasionado por el desarrollo de sofware, con gran gusto en construir
           cualquier cosa con codigo y hacer aplicaciones facinantes.
         </span>
-        <div className="flex gap-6 mt-4">
-          <button className="border-2 border-solid px-6 py-2 text-md rounded-3xl text-gray-1F border-gray-1F dark:text-gray-300 dark:border-gray-300">
-            Ver mi CV
-          </button>
-          <button className="border-2 border-solid px-6 py-2 text-md rounded-3xl text-gray-1F border-gray-1F dark:text-gray-300 dark:border-gray-300">
-            Explora mi web
-          </button>
+        <div className="mt-6 flex flex-col sm:flex-row gap-3">
+          <a
+            href="https://web3templates.com/templates/astroship-starter-website-template-for-astro"
+            target="_blank"
+            rel="noopener"
+            className="rounded-3xl text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-5 py-2.5 hover:bg-black text-white bg-gray-1F border-2 border-transparent flex gap-1 items-center justify-center"
+          >
+            <svg
+              viewBox="0 0 24 24"
+              className="text-white w-5 h-5"
+              astro-icon="bx:bxs-cloud-download"
+            >
+              <path
+                fill="currentColor"
+                d="M18.944 11.112C18.507 7.67 15.56 5 12 5 9.244 5 6.85 6.61 5.757 9.149 3.609 9.792 2 11.82 2 14c0 2.657 2.089 4.815 4.708 4.971V19H17.99v-.003L18 19c2.206 0 4-1.794 4-4a4.008 4.008 0 0 0-3.056-3.888zM8 12h3V9h2v3h3l-4 5-4-5z"
+              ></path>
+            </svg>
+            Descargar CV
+          </a>
+          <a
+            href="https://github.com/surjithctly/astroship"
+            rel="noopener"
+            target="_blank"
+            className="rounded-3xl text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-5 py-2.5 bg-white border-2 border-black hover:bg-gray-100 text-black flex gap-1 items-center justify-center"
+          >
+            <svg
+              viewBox="0 0 24 24"
+              className="text-black w-4 h-4"
+              astro-icon="bx:bxl-github"
+            >
+              <path
+                fill="currentColor"
+                fill-rule="evenodd"
+                d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            GitHub Repo
+          </a>
         </div>
       </div>
     </div>
--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -3,9 +3,11 @@ import Flex from "@/shared/UIComponents/Base/Flex";
 import P from "@/shared/UIComponents/Base/P";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import { cn } from "tailwind-cn";
 import Logo from "../Logo/Index";
 import Switcher from "../SwitcherDark";
+import Sidebar from "./Sidebar";
 
 const paths = [
   {
@@ -30,6 +32,8 @@ const paths = [
   },
 ];
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   const fulpath = usePathname();
   const statePage: any = (path: any) => {
     if (path.startsWith("/blog")) {
@@ -62,71 +66,79 @@ const Header = () => {
     };
   };
   const path = statePage(fulpath);
-  
- 
-  return (
-    <Flex full col className="lg:max-w-screen sticky  top-0 z-30  sm:bg-opacity-90 sm:backdrop-blur-lg">
-      <Flex full itemscenter between className="mx-auto px-8 py-0">
-        <Flex>
-          <div className="relative flex items-center z-10  self-stretch  lg:pr-8">
-            <Link className="focus:outline-none" href="/">
-              <Logo />
-            </Link>
-            <Link
-              className="ml-2 text-xl flex font-bold text-graydark dark:text-graywhite space-x-1"
-              href={path?.path}
-            >
-              {path?.path !== "/" && <span>/</span>}
-              <span>{path?.name}</span>
-            </Link>
-          </div>
-        </Flex>
-        <Flex full className="hidden lg:flex space-x-8 ml-auto justify-end">
-          {paths.map((el: any, index: any) => {
-            return (
-              <Link key={index} href={el.path} className="">
-                <P className={cn("text-sm", "")}>{el.text}</P>
-              </Link>
-            );
-          })}
-        </Flex>
-        <Flex itemscenter className="space-x-4">
-          <button
-            className="lg:hidden w-12 h-12 relative -mr-2 z-20"
-            aria-label="Toggle menu"
-          >
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-8 w-8 absolute top-2 left-2 transition-opacity"
-            >
-              <path
-                d="M4 7h16M4 12h16M4 17h16"
-                stroke="currentColor"
-                strokeWidth="1.5"
-              />
-            </svg>
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-8 w-8 absolute top-2 left-2 transition-opacity opacity-0"
-            >
-              <path
-                d="m7.757 7.757 8.486 8.486M7.757 16.243l8.486-8.486"
-                stroke="currentColor"
-                strokeWidth="1.5"
-              />
-            </svg>
-          </button>
 
-          <div>
-            <Switcher />
-          </div>
+  return (
+    <>
+      <Flex
+        full
+        col
+        className="lg:max-w-screen sticky  top-0 z-30  sm:bg-opacity-90 sm:backdrop-blur-lg"
+      >
+        <Flex full itemscenter between className="mx-auto px-8 py-0">
+          <Flex>
+            <div className="relative flex items-center z-10  self-stretch  lg:pr-8">
+              <Link className="focus:outline-none" href="/">
+                <Logo />
+              </Link>
+              <Link
+                className="ml-2 text-xl flex font-bold text-graydark dark:text-graywhite space-x-1"
+                href={path?.path}
+              >
+                {path?.path !== "/" && <span>/</span>}
+                <span>{path?.name}</span>
+              </Link>
+            </div>
+          </Flex>
+          <Flex full className="hidden lg:flex space-x-8 ml-auto justify-end">
+            {paths.map((el: any, index: any) => {
+              return (
+                <Link key={index} href={el.path} className="">
+                  <P className={cn("text-sm", "")}>{el.text}</P>
+                </Link>
+              );
+            })}
+          </Flex>
+          <Flex itemscenter className="space-x-4">
+            <button
+              className="lg:hidden w-12 h-12 relative -mr-2 z-20"
+              aria-label="Toggle menu"
+              onClick={() => setOpen(true)}
+            >
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8 absolute top-2 left-2 transition-opacity"
+              >
+                <path
+                  d="M4 7h16M4 12h16M4 17h16"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                />
+              </svg>
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8 absolute top-2 left-2 transition-opacity opacity-0"
+              >
+                <path
+                  d="m7.757 7.757 8.486 8.486M7.757 16.243l8.486-8.486"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                />
+              </svg>
+            </button>
+
+            <div>
+              <Switcher />
+            </div>
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+
+      <Sidebar open={open} setOpen={setOpen} />
+    </>
   );
 };
 
