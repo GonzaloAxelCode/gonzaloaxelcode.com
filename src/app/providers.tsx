5f512a620310fad4e267cc713356af9488c55502
GonzaloAxelCode <gonzaloaxelcode@gmail.com>
2023-11-14 08:40:04 -0500

    adasdddddddddddddddddddddddddddd

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@ import { UIProvider } from "@/shared/contexts/UIContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const GlobalProviders = ({ children }: any) => {
+  //xd
   return (
     <UIProvider>
       <GoogleOAuthProvider clientId="157232541478-1dgf60smuro9tevfo0o4lbg5igk7jmnm.apps.googleusercontent.com">
