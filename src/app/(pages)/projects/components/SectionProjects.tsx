e84a5d9c79c5a50f02900ff6cc3976026a8feced
GonzaloAxelCode <gonzaloaxelcode@gmail.com>
2024-05-01 17:39:20 -0500

    Completado el blog al 98%

--- src/app/(pages)/projects/components/SectionProjects.tsx
+++ src/app/(pages)/projects/components/SectionProjects.tsx
@@ -58,32 +58,41 @@ const SectionProjects = () => {
                               />
                               <span>Acceso Privado</span>
                             </div>
-
-                            <button className="cursor-pointer flex items-center gap-2  px-4 py-2  w-fi">
-                              <Link
-                                target="_blank"
-                                href={linkpreview}
-                                className=" hover:underline"
-                              >
+                            {linkpreview === "#" && (
+                              <button className="cursor-pointer flex items-center gap-2  px-4 py-2  w-fi">
                                 <span className="flex items-center body-medium font-sans !font-medium ">
-                                  Live Preview
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    id="root"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                    className="w-5 h-5"
-                                    aria-hidden="true"
-                                  >
-                                    <path
-                                      fillRule="evenodd"
-                                      d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                                      clipRule="evenodd"
-                                    />
-                                  </svg>
+                                  En desarrollo aun
                                 </span>
-                              </Link>
-                            </button>
+                              </button>
+                            )}
+
+                            {linkpreview !== "#" && (
+                              <button className="cursor-pointer flex items-center gap-2  px-4 py-2  w-fi">
+                                <Link
+                                  target="_blank"
+                                  href={linkpreview}
+                                  className=" hover:underline"
+                                >
+                                  <span className="flex items-center body-medium font-sans !font-medium ">
+                                    Live Preview
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      id="root"
+                                      viewBox="0 0 20 20"
+                                      fill="currentColor"
+                                      className="w-5 h-5"
+                                      aria-hidden="true"
+                                    >
+                                      <path
+                                        fillRule="evenodd"
+                                        d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                        clipRule="evenodd"
+                                      />
+                                    </svg>
+                                  </span>
+                                </Link>
+                              </button>
+                            )}
                           </div>
                         ) : null}
                         {!isLocked ? (
